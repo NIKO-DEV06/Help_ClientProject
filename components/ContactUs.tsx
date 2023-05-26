dae0ce2ba67e9770daf91f4430cd20da2a0a7f17
Ayeniko Emmanuel <ayenikoemmanuel06@gmail.com>
2023-05-26 19:00:26 +0100

    added contact section

--- components/ContactUs.tsx
+++ components/ContactUs.tsx
@@ -1,9 +1,9 @@
 const ContactUs = () => {
   return (
     <section className="pb-[4rem]">
-      <h2 className="text-center text-[#2B4E8C] tracking-wider">Join Us</h2>
+      <h2 className="text-center text-[#2B4E8C] tracking-wider">Contacts</h2>
       <h1 className="text-center text-[1.5rem] font-semibold pb-[2rem]">
-        Our App
+        Contact us
       </h1>
     </section>
   );
