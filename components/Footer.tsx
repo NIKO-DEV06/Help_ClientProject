358cd8da2fb4195c3ae0b4346f7d0ec10af96b33
Ayeniko Emmanuel <ayenikoemmanuel06@gmail.com>
2023-05-27 17:42:06 +0100

    added footer and mobile menu

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,5 +1,71 @@
+import logo from "@/assets/image-PhotoRoom (12) 1.png";
+import appStore from "@/assets/image 1.png";
+import playStore from "@/assets/image 2.png";
+import Link from "next/link";
+import Image from "next/image";
+import "../app/globals.css";
+
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className="grid md:grid-cols-2 lg:grid-cols-4 place-items-center md:place-items-start gap-[2rem] px-[5rem] pb-[3rem] md:scale-95">
+      <div className="flex flex-col gap-[1rem] text-center md:text-left">
+        <Link href={"/"} className="mx-auto md:mx-0">
+          <Image src={logo} height={100} width={100} alt="logo-image" />
+        </Link>
+        <p className="opacity-60">
+          Make your to-do list wherever <br /> you are with our mobile app.
+        </p>
+        <p className="opacity-60">&copy;2023. All rights reserved.</p>
+      </div>
+      <div className="flex flex-col gap-[0.5rem] text-center md:text-left">
+        <h2 className="font-bold text-[#2B4E8C]">About Us</h2>
+        <Link className="link-with-underline" href={"/"}>
+          <p className="opacity-60">Frequently asked questions</p>
+        </Link>
+        <Link className="link-with-underline" href={"/"}>
+          <p className="opacity-60">Confidentiality policy </p>
+        </Link>
+        <Link className="link-with-underline" href={"/"}>
+          <p className="opacity-60">Contact</p>
+        </Link>
+        <Link className="link-with-underline" href={"/"}>
+          <p className="opacity-60">Blog</p>
+        </Link>
+      </div>
+      <div className="flex flex-col gap-[0.5rem] text-center md:text-left">
+        <h2 className="font-bold text-[#2B4E8C]">Contacts</h2>
+        <p className="opacity-60">+351-965-990-000</p>
+        <p className="opacity-60">@helpyou@gmail.com</p>
+      </div>
+      <div className="flex flex-col gap-[0.5rem] text-center md:text-left">
+        <h2 className="font-bold text-[#2B4E8C]">Download our app</h2>
+        <p className="opacity-60">
+          Tackle your to-do list wherever you are
+          <br className="hidden md:flex" /> with our mobile app.
+        </p>
+        <div className="flex mt-[1.5rem] gap-[1rem] mx-auto md:mx-0">
+          <button>
+            <Image
+              src={appStore}
+              height={120}
+              width={120}
+              alt="appstore"
+              className="cursor-pointer h-[2.7rem]"
+            />
+          </button>
+          <button>
+            <Image
+              src={playStore}
+              height={120}
+              width={120}
+              alt="playstore"
+              className="cursor-pointe  h-[2.7rem]"
+            />
+          </button>
+        </div>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,36 +1,142 @@
+"use client";
+
+import { Fragment } from "react";
 import Image from "next/image";
 import Link from "next/link";
+import { useState } from "react";
+import { motion, AnimatePresence } from "framer-motion";
 import logo from "@/assets/image-PhotoRoom (12) 1.png";
+import cancel from "@/assets/cancel2-svgrepo-com.svg";
 import menu from "@/assets/burger-menu-svgrepo-com.svg";
 import "../app/globals.css";
 
 const Header = () => {
+  const [isNav, setIsNav] = useState(false);
+  const openNav = () => {
+    setIsNav(true);
+    console.log("opened");
+  };
+  const closeNav = () => {
+    setIsNav(false);
+  };
+
+  const navVariants = {
+    hidden: {
+      x: "100%",
+    },
+    visible: {
+      x: "0%",
+      transition: {
+        duration: 0.3,
+        ease: "easeInOut",
+        type: "spring",
+        damping: 25,
+        stiffness: 400,
+      },
+    },
+    exit: {
+      x: "100%",
+      transition: {
+        duration: 0.3,
+        ease: "easeInOut",
+      },
+    },
+  };
   return (
-    <header className="fixed bg-white flex justify-between md:justify-evenly w-screen px-[1.5rem] py-[1rem] md:py-[1.5rem] items-center border-b-[1px] z-50">
-      <Link href={"/"}>
-        <Image src={logo} height={100} width={100} alt="logo-image" />
-      </Link>
-      <div className="md:hidden">
-        <Image src={menu} height={35} width={35} alt="menu-image" />
-      </div>
-      <div className="hidden md:flex tracking-wider gap-[2rem] lg:gap-[2.5rem]">
-        <Link className="link-with-underline" href={"/"}>
-          Home
+    <>
+      <header className="fixed bg-white flex justify-between md:justify-evenly w-screen px-[1.5rem] py-[1rem] md:py-[1.5rem] items-center border-b-[1px] z-50">
+        <Link href={"/"}>
+          <Image src={logo} height={100} width={100} alt="logo-image" />
         </Link>
-        <Link className="link-with-underline" href={"/"}>
-          Contact
-        </Link>
-        <Link className="link-with-underline" href={"/"}>
-          Confidentiality policy{" "}
-        </Link>
-        <Link className="link-with-underline" href={"/"}>
-          About Us
-        </Link>
-        <Link className="link-with-underline" href={"/"}>
-          Blog
-        </Link>
-      </div>
-    </header>
+        <div className="md:hidden" onClick={openNav}>
+          <Image src={menu} height={35} width={35} alt="menu-image" />
+        </div>
+        <div className="hidden md:flex tracking-wider gap-[2rem] lg:gap-[2.5rem]">
+          <Link className="link-with-underline" href={"/"}>
+            Home
+          </Link>
+          <Link className="link-with-underline" href={"/"}>
+            Contact
+          </Link>
+          <Link className="link-with-underline" href={"/"}>
+            Confidentiality policy
+          </Link>
+          <Link className="link-with-underline" href={"/"}>
+            About Us
+          </Link>
+          <Link className="link-with-underline" href={"/"}>
+            Blog
+          </Link>
+        </div>
+      </header>
+      <AnimatePresence initial={true} mode="wait">
+        {isNav && (
+          <Fragment>
+            <motion.div
+              key={Math.random()}
+              initial="hidden"
+              animate="visible"
+              exit="exit"
+              variants={navVariants}
+              className="fixed md:hidden top-0 bottom-0 right-0 z-[70] left-[30%] bg-[#0000007F] backdrop-blur-[2rem] flex flex-col gap-[2.5rem] md:font-[300] uppercase tracking-wider text-[0.95rem] py-[10rem] px-[2rem] text-white"
+            >
+              <Image
+                onClick={closeNav}
+                src={cancel}
+                alt="cancel-svg"
+                width={40}
+                height={40}
+                className="fixed right-[1.5rem] top-[1.5rem] cursor-pointer"
+              />
+              <Link
+                onClick={closeNav}
+                className="flex gap-[0.5rem] cursor-pointer"
+                href={"/"}
+              >
+                <span className="font-bold">00</span> Home
+              </Link>
+              <Link
+                onClick={closeNav}
+                className="flex gap-[0.5rem] cursor-pointer"
+                href={"/"}
+              >
+                <span className="font-bold">01</span> Contact
+              </Link>
+              <Link
+                onClick={closeNav}
+                className="flex gap-[0.5rem] cursor-pointer"
+                href={"/"}
+              >
+                <span className="font-bold text-sm">02</span> Confidentiality
+                policy
+              </Link>
+              <Link
+                onClick={closeNav}
+                className="flex gap-[0.5rem] cursor-pointer"
+                href={"/"}
+              >
+                <span className="font-bold">03</span> About Us
+              </Link>
+              <Link
+                onClick={closeNav}
+                className="flex gap-[0.5rem] cursor-pointer"
+                href={"/"}
+              >
+                <span className="font-bold">04</span> Blog
+              </Link>
+            </motion.div>
+            <motion.div
+              key="backdrop"
+              className="fixed md:hidden bg-black inset-0 z-[60] opacity-40"
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 0.4 }}
+              exit={{ opacity: 0 }}
+              onClick={closeNav}
+            ></motion.div>
+          </Fragment>
+        )}
+      </AnimatePresence>
+    </>
   );
 };
 
